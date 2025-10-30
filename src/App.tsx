40eb6d1d6a93dd877370f0edb4464fedcaad11f8
Fabrício <fabricioperesdsantos@gmail.com>
2025-10-30 15:42:42 -0300

    Adicionando rota de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NaoEncontrada from "./paginas/NaoEncontrada"
+import Login from "./paginas/Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        <Route path="login" element={ <Login /> } />
+        
         <Route path="*" element={ <NaoEncontrada /> } />
       </Routes>
     </BrowserRouter>
