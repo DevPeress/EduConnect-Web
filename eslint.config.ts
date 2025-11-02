02176f7612c84d3f35ebe29a35089f5787104d0b
Peres <fabricioperesdsantos@gmail.com>
2025-11-01 21:42:53 -0300

    Remoção do prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,11 +3,9 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-plugin-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  // Ignora a pasta de build
   globalIgnores(["dist"]),
 
   {
@@ -15,15 +13,11 @@ export default defineConfig([
     ignores: ["dist", "node_modules"],
 
     extends: [
-      js.configs.recommended,               // Regras JS padrão
-      tseslint.configs.recommended,         // Regras TS
-      reactHooks.configs["recommended-latest"], // Boas práticas de hooks
-      reactRefresh.configs.vite,            // Regras específicas do Vite
+      js.configs.recommended,            
+      tseslint.configs.recommended,         
+      reactHooks.configs["recommended-latest"], 
+      reactRefresh.configs.vite,           
     ],
-
-    plugins: {
-      prettier, // Integra o Prettier
-    },
 
     languageOptions: {
       ecmaVersion: "latest",
@@ -32,23 +26,8 @@ export default defineConfig([
     },
 
     rules: {
-      // Regras Prettier
-      "prettier/prettier": [
-        "error",
-        {
-          semi: true,             // exige ponto e vírgula
-          singleQuote: false,     // usa aspas duplas
-          trailingComma: "es5",   // vírgula em objetos/arrays multilinha
-          printWidth: 80,         // quebra de linha
-          tabWidth: 2,
-          bracketSpacing: true,
-          endOfLine: "auto",
-        },
-      ],
-
-      // Outras boas práticas
       "no-unused-vars": "warn",
-      "react-refresh/only-export-components": "off", // opcional para dev
+      "react-refresh/only-export-components": "off",
     },
   },
 ]);
