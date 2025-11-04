8546b1742507af63e75ecd902835018769e88b9d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-04 17:54:18 -0300

    Criação da NavBar para Administradores

---
+++ src/components/Administracao/navBar/index.tsx
@@ -0,0 +1,79 @@
+import { Link, useLocation } from "react-router-dom";
+import DashBoard from '../../../assets/DashBoard.svg';
+import Alunos from '../../../assets/Alunos.svg';
+import Professores from '../../../assets/Professores.svg';
+import Turmas from '../../../assets/Turmas.svg';
+import type { NavAdmin } from "../../../types/types";
+
+const NavBarAdmin = () => {
+  const location = useLocation();
+  const Principal: NavAdmin[] = [
+    { to: "/login", nome: "DashBoard", pagina: "/admin", svg: DashBoard },
+    { to: "/login", nome: "Alunos", pagina: "/admin/alunos", svg: Alunos },
+    { to: "/login", nome: "Professores", pagina: "/admin/professores", svg: Professores },
+    { to: "/login", nome: "Turmas", pagina: "/admin/turmos", svg: Turmas },
+  ];
+
+  const Academico: NavAdmin[] = [
+    { to: "/login", nome: "Notas", pagina: "/admin/notas", svg: DashBoard },
+    { to: "/login", nome: "Calendário", pagina: "/admin/calendario", svg: Alunos },
+    { to: "/login", nome: "Financeiro", pagina: "/admin/financeiro", svg: Professores },
+    { to: "/login", nome: "Relatórios", pagina: "/admin/relatorio", svg: Turmas },
+  ];
+
+  return (
+    <nav className="flex-1 px-6 py-6">
+        <div className="mb-6">
+            <span className="block text-[11px] font-semibold text-(--text-muted) uppercase leading-[0.5px] px-2 py-2 mb-2">Principal</span>
+            {Principal.map((item) => {
+                const paginaAtual: boolean = location.pathname === item.pagina;
+
+                return (
+                    <Link
+                        key={item.nome}
+                        to={"/login"}
+                        className="flex items-center gap-3 px-[11px] py-3 rounded-[10px] text-[14px] font-medium relative hover:bg-(--bg-hover) hover:text-(--text-primary)"
+                        style={{
+                            color: paginaAtual ? "white" : "var(--text-secondary)",
+                            boxShadow: paginaAtual
+                            ? "0 4px 12px rgba(59, 130, 246, 0.3)"
+                            : "",
+                            background: paginaAtual ? "var(--primary-color)" : "",
+                        }}
+                    >
+                    <img className="w-5 h-5" src={item.svg} alt="" />
+                    <span>{item.nome}</span>
+                    </Link>
+                );
+            })}
+        </div>
+
+        <div className="mb-6">
+            <span className="block text-[11px] font-semibold text-(--text-muted) uppercase leading-[0.5px] px-2 py-2 mb-2">Acadêmico</span>
+            {Academico.map((item) => {
+                const paginaAtual: boolean = location.pathname === item.pagina;
+
+                return (
+                    <Link
+                        key={item.nome}
+                        to={"/login"}
+                        className="flex items-center gap-3 px-[11px] py-3 rounded-[10px] text-[14px] font-medium relative hover:bg-(--bg-hover) hover:text-(--text-primary)"
+                        style={{
+                            color: paginaAtual ? "white" : "var(--text-secondary)",
+                            boxShadow: paginaAtual
+                            ? "0 4px 12px rgba(59, 130, 246, 0.3)"
+                            : "",
+                            background: paginaAtual ? "var(--primary-color)" : "",
+                        }}
+                    >
+                    <img className="w-5 h-5" src={item.svg} alt="" />
+                    <span>{item.nome}</span>
+                    </Link>
+                );
+            })}
+        </div>
+    </nav>
+  );
+};
+
+export default NavBarAdmin;
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -6,33 +6,40 @@ export interface ThemeType {
 export interface LoginType {
   email: string;
   senha: string;
-}
+};
 
 export interface PrivateRouteProps {
   isAuthenticated: boolean;
   userRole: string; 
   allowedRoles: string[]; 
   redirectTo?: string;
-}
+};
 
 export interface LoginResponse {
   cargo: string;
   token: string;
-}
+};
 
 export interface LoginProps {
   TrocarInfos: (cargo: string, token: string) => void
-}
+};
 
 export interface AuthContextType {
   cargo: string;
   token: string;
   setAuth: (cargo: string, token: string) => void;
-}
+};
 
 export interface Acessos {
   logado: boolean;
   pagina: string;
   mensagem: string;
   cargos?: string[];
+};
+
+export interface NavAdmin {
+  to: string,
+  nome: string,
+  pagina: string,
+  svg: string
 }
