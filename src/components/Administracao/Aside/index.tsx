7975bafa001ae427464569db5db5cbbfa5def14f
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-06 13:43:40 -0300

    Jogando aside como componente para requisitar em outras páginas

---
+++ src/components/Administracao/Aside/index.tsx
@@ -0,0 +1,21 @@
+import SidebarFooter from "../../SideBar-Footer";
+import NavBarAdmin from "../NavBar/NavBarAdmin";
+import Logo from '../../../../public/Logo.svg'
+
+const AsideAdmin = () => {
+  return (
+    <aside className="flex flex-col fixed h-full w-72 bg-(--bg-sidebar) border-r-2 border-(--border-color)">
+      <div className="px-6 py-6 border-b-2 border-(--border-color)">
+        <div className="flex items-center gap-3 text-[18px] font-bold text-(--text-primary)">
+            <img className="w-5" src={Logo} alt="" />
+          <span>EduManager</span>
+        </div>
+      </div>
+
+      <NavBarAdmin />
+      <SidebarFooter />
+    </aside>
+  );
+};
+
+export default AsideAdmin;
--- src/paginas/Administracao/Inicio/index.tsx
+++ src/paginas/Administracao/Inicio/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import CardsAdmin from "../../../components/Administracao/CardsAdmin";
-import NavBarAdmin from "../../../components/Administracao/NavBar/NavBarAdmin";
 import Header from "../../../components/Header";
-import SidebarFooter from "../../../components/SideBar-Footer";
 import type { CardsAdminType } from "../../../types/types";
+import AsideAdmin from "../../../components/Administracao/Aside";
 
 const InicioAdm = () => {
   const [dados] = useState<CardsAdminType[]>([
@@ -15,16 +14,7 @@ const InicioAdm = () => {
 
   return (
     <div className="flex min-h-full overflow-hidden">
-      <aside className="flex flex-col fixed h-full w-72 bg-(--bg-sidebar) border-r-2 border-(--border-color)">
-        <div className="px-6 py-6 border-b-2 border-(--border-color)">
-          <div className="flex items-center gap-3 text-[18px] font-bold text-(--text-primary)">
-            <span>EduManager</span>
-          </div>
-        </div>
-
-        <NavBarAdmin />
-        <SidebarFooter />
-      </aside>
+      <AsideAdmin />
 
       <main className="flex-1 ml-72 flex flex-col bg-(--bg-body)">
         <Header />
