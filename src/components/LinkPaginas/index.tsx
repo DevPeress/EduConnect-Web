f8037faf98d21f63fb48345df8dbb8a61b56452a
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-07 18:33:54 -0300

    Melhorando a cor dos textos

--- src/components/LinkPaginas/index.tsx
+++ src/components/LinkPaginas/index.tsx
@@ -13,7 +13,7 @@ const LinkPaginas = ({
       to={item.pagina}
       className="flex items-center gap-3 px-[11px] py-3 rounded-[10px] text-[14px] font-medium relative hover:bg-(--bg-hover) hover:text-(--text-primary)"
       style={{
-        color: paginaAtual ? "white" : "var(--text-secondary)",
+        color: paginaAtual ? "white" : "var(--text-primary)",
         boxShadow: paginaAtual ? "0 4px 12px rgba(59, 130, 246, 0.3)" : "",
         background: paginaAtual ? "var(--primary-color)" : "",
       }}
