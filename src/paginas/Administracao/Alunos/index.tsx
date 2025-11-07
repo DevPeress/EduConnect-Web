6bfb4f61b7092a226ae75a330985e19ddf6b0b4b
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-07 13:37:14 -0300

    Criação da Pagina de Alunos

---
+++ src/paginas/Administracao/Alunos/index.tsx
@@ -0,0 +1,13 @@
+import AsideAdmin from "../../../components/Administracao/AsideAdmin";
+
+const AlunosAdmin = () => {
+  return (
+    <div className="flex min-h-full">
+      <AsideAdmin />
+
+      
+    </div>
+  );
+};
+
+export default AlunosAdmin;
