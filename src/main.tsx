e226b3c8301bb9e484d02b71b3b53221fd9318b2
Peres <fabricioperesdsantos@gmail.com>
2025-11-09 19:34:59 -0300

    Adição do Provider do Cadastro do Aluno

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext/index.tsx'
 import { AuthProvider } from './context/AuthContext/index.tsx'
+import { CadastroAlunoProvider } from './context/CadastroAlunoContext/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider> 
+    <ThemeProvider>
       <AuthProvider>
-        <App />
+        <CadastroAlunoProvider>
+          <App />
+        </CadastroAlunoProvider>
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
