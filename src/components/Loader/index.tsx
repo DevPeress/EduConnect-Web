34cfe2cae8589816a2209d10f69ef770dd1f64ef
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-10 10:16:15 -0300

    Melhorando a estilização do loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from "react";
 
 const Loader = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full h-full items-center justify-center">
-      <div className="w-12 h-12 border-4 border-[#d1d5db] border-t-[#3b82f6] rounded-full animate-spin mx-auto mt-20" />
-      <h1 className="text-[13px] text-(--text-primary)">{children}</h1>
+    <div className="flex flex-col items-center justify-center top-0 bottom-0 left-0 right-0 m-auto select-none">
+      <div className="w-12 h-12 border-4 border-[#d1d5db] border-t-[#3b82f6] rounded-full animate-spin mx-auto mt-20 mb-10" />
+      <h1 className="text-[15px] text-(--text-primary)">{children}</h1>
     </div>
   );
 };
