8a9d07ad51275732278efa2fd4fc4cee31fdde4d
Peres <fabricioperesdsantos@gmail.com>
2025-11-09 23:15:15 -0300

    Ajuste do titulo para children

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react";
 
-const Loader = ({ titulo }: { titulo: ReactNode }) => {
+const Loader = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full h-full items-center justify-center">
       <div className="w-12 h-12 border-4 border-[#d1d5db] border-t-[#3b82f6] rounded-full animate-spin mx-auto mt-20" />
-      <h1 className="text-[13px] text-(--text-primary)">{titulo}</h1>
+      <h1 className="text-[13px] text-(--text-primary)">{children}</h1>
     </div>
   );
 };
