4d344a708e85d7fdf7a0f742334ebeca93b88e7b
Peres <fabricioperesdsantos@gmail.com>
2025-11-09 22:06:25 -0300

    Colocando o Provider do Cadastro do Professor

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext/index.tsx'
 import { AuthProvider } from './context/AuthContext/index.tsx'
 import { CadastroAlunoProvider } from './context/CadastroAlunoContext/index.tsx'
+import { CadastroProfessorProvider } from './context/CadastroProfessorContext/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
         <CadastroAlunoProvider>
-          <App />
+          <CadastroProfessorProvider>
+            <App />
+          </CadastroProfessorProvider>
         </CadastroAlunoProvider>
       </AuthProvider>
     </ThemeProvider>
