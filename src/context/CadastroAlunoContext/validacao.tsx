867e442a83b2a10f5b3d1f2077d87298414c428d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-10 10:04:36 -0300

    Criação do código para validar dados do form

---
+++ src/context/CadastroAlunoContext/validacao.tsx
@@ -0,0 +1,39 @@
+import toast from "react-hot-toast";
+import type { CadastroAlunoType } from "../../types/types";
+
+const ValidarAluno = (dados: CadastroAlunoType) => {
+  const email = dados.email;
+  const nome = dados.nome;
+  const telefone = dados.telefone;
+  const endereco = dados.endereco;
+  const nascimento = dados.nascimento;
+
+  if (!nome || nome.length < 3) {
+    toast.error("Nome inválido!");
+    return false;
+  }
+
+  if (!nascimento) {
+    toast.error("Data de Nascimento inválida!");
+    return false;
+  }
+
+  if (!email.includes("@") && !email.includes(".com")) {
+    toast.error("E-mail inválido!");
+    return false;
+  }
+
+  if (!telefone || telefone.length < 14) {
+    toast.error("Telefone inválido!");
+    return false;
+  }
+
+  if (!endereco || endereco.length < 5) {
+    toast.error("Endereço inválido!");
+    return false;
+  }
+
+  return true;
+};
+
+export default ValidarAluno;
