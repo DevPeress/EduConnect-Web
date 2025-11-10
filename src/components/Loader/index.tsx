ac9a5b68c7f80690f000124a71940043efdcd08f
Peres <fabricioperesdsantos@gmail.com>
2025-11-09 22:53:11 -0300

    Criação do loader para as paginas

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+
+const Loader = ({ titulo }: { titulo: ReactNode }) => {
+  return (
+    <div className="w-full h-full items-center justify-center">
+      <div className="w-12 h-12 border-4 border-[#d1d5db] border-t-[#3b82f6] rounded-full animate-spin mx-auto mt-20" />
+      <h1 className="text-[13px] text-(--text-primary)">{titulo}</h1>
+    </div>
+  );
+};
+
+export default Loader;
