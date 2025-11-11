013efe74bdee6193397d543ad981084a6dacc96a
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-11 09:44:35 -0300

    Ajustando css do loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,8 +3,8 @@ import type { ReactNode } from "react";
 const Loader = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col items-center justify-center select-none">
-      <div className="w-20 h-20 border-4 border-[#d1d5db] border-t-[#3b82f6] rounded-full animate-spin mx-auto mb-10" />
-      <h1 className="text-[15px] text-(--text-primary)">{children}</h1>
+      <div className="w-35 h-35 border-10 border-[#d1d5db] border-t-[#3b82f6] rounded-full animate-spin mx-auto mb-10" />
+      <h1 className="text-[20px] text-(--text-primary)">{children}</h1>
     </div>
   );
 };
