3a60c0923b7177cf345e969b2553da32d6310b2e
Peres <fabricioperesdsantos@gmail.com>
2025-11-13 23:04:04 -0300

    docs(paginação): Documentação adicionada ao arquivo de paginação

--- src/utils/paginação/index.ts
+++ src/utils/paginação/index.ts
@@ -1,5 +1,8 @@
 import type { Acessos } from "../../types/types";
 
+// Função responsável por verificar se o usuário está logado. 
+// Caso não esteja, redireciona para a página de login. 
+// Se estiver autenticado, valida o cargo e redireciona para a página correspondente.
 export const Options: Acessos[] = [
     { logado: false, pagina: "/login", mensagem: "Ir para o Login" },
     { logado: true, pagina: "/login", mensagem: "Ir para Área Administrativa", cargos: ["Admin"] },
