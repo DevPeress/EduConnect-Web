c8a354929bcb91f80de822a6d46af1e83854a548
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-18 17:22:38 -0300

    Colocando todos os imports no arquivo geral de export

--- src/assets/HTML/index.tsx
+++ src/assets/HTML/index.tsx
@@ -6,6 +6,16 @@ import { TotalAlunos } from "./arquivos/TotalAlunos";
 import { TotalProfessores } from "./arquivos/TotalProfessores";
 import { TotalTurmas } from "./arquivos/TotalTurmas";
 import { TotalReceita } from "./arquivos/TotalReceita";
+import { NavAlunos } from "./arquivos/NavBar/NavAluno";
+import { NavCalendario } from "./arquivos/NavBar/NavCalendario";
+import { NavConfig } from "./arquivos/NavBar/NavConfig";
+import { NavDashBoard } from "./arquivos/NavBar/NavDashBoard";
+import { NavFinanceiro } from "./arquivos/NavBar/NavFinanceiro";
+import { NavNotas } from "./arquivos/NavBar/NavNotas";
+import { NavProfessores } from "./arquivos/NavBar/NavProfessores";
+import { NavRelatorios } from "./arquivos/NavBar/NavRelatorios";
+import { NavSair } from "./arquivos/NavBar/NavSair";
+import { NavTurmas } from "./arquivos/NavBar/NavTurmas";
 
 export {
   CadastrarAluno,
@@ -16,4 +26,14 @@ export {
   TotalProfessores,
   TotalTurmas,
   TotalReceita,
+  NavAlunos,
+  NavCalendario,
+  NavConfig,
+  NavDashBoard,
+  NavFinanceiro,
+  NavNotas,
+  NavProfessores,
+  NavRelatorios,
+  NavSair,
+  NavTurmas
 };
