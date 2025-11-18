15fc348884da2a456025b2f8a5afcc6aa03dbc48
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-18 17:10:05 -0300

    Inserindo Arquivo padrão para export dos icones svg criados via HTML

---
+++ src/assets/HTML/index.tsx
@@ -0,0 +1,6 @@
+import { Aluno } from "./arquivos/Aluno";
+import { Nota } from "./arquivos/Nota";
+import { Receita } from "./arquivos/Receita";
+import { Relatorio } from "./arquivos/Relatorio";
+
+export { Aluno, Nota, Receita, Relatorio };
