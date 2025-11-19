d989d8869556393207ff2ede498aabfeebd7c7af
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 14:33:01 -0300

    Criando arquivos fora das pastas

---
+++ src/components/Cadastros/flex-1.tsx
@@ -0,0 +1,64 @@
+import type { CadastroAlunoInput } from "../../schemas/alunoSchema";
+import type { CadastroProfessorInput } from "../../schemas/professorSchema";
+import type { CadastroFlexProps } from "../../types/types";
+import { IdentificarTipo } from "../../utils/codigos";
+
+interface CadastroFlex1Prop<
+  T extends CadastroAlunoInput | CadastroProfessorInput
+> extends CadastroFlexProps<T> {
+  titulo: string;
+  place: string;
+}
+
+const CadastroFlex1 = <T extends CadastroAlunoInput | CadastroProfessorInput>({
+  titulo,
+  infos,
+  setInfos,
+  place,
+}: CadastroFlex1Prop<T>) => {
+  // Processa a opção recebida e retorna o resultado conforme o contexto de criação de alunos ou professores.
+  const tipo = IdentificarTipo(titulo) as keyof T;
+
+  // Cria o Select ou Input para ser demonstrado
+  const TipoDiv = () => {
+    switch (tipo) {
+      case "nome":
+      case "endereco":
+        return (
+          <input
+            value={infos[tipo] as string}
+            onChange={(e) =>
+              setInfos((prevDados) => ({
+                ...prevDados,
+                [tipo]: e.target.value,
+              }))
+            }
+            type={tipo === "endereco" ? "textarea" : "text"}
+            className="w-full py-3 px-4 bg-(--bg-input) border-2 border-(--border-color) rounded-[10px] text-(--text-primary) text-[14px] focus:outline-none focus:border-(--primary-color)"
+            id={String(tipo)}
+            name={String(tipo)}
+            placeholder={place}
+            required
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-1 gap-5 mb-5">
+      <div className="flex flex-col gap-2">
+        <label
+          className="text-[14px] font-semibold text-(--text-primary)"
+          htmlFor={String(tipo)}
+        >
+          {titulo} <span className="text-(--red) ml-0.5">*</span>
+        </label>
+        {TipoDiv()}
+      </div>
+    </div>
+  );
+};
+
+export default CadastroFlex1;
---
+++ src/components/Cadastros/flex-2.tsx
@@ -0,0 +1,134 @@
+import type { CadastroAlunoInput } from "../../schemas/alunoSchema";
+import type { CadastroProfessorInput } from "../../schemas/professorSchema";
+import type { CadastroFlexProps } from "../../types/types";
+import { formatCPF, formatTelefone, IdentificarTipo } from "../../utils/codigos";
+
+interface CadastroFlex2Prop<
+  T extends CadastroAlunoInput | CadastroProfessorInput
+> extends CadastroFlexProps<T> {
+  opcao1: string;
+  opcao2: string;
+}
+
+const CadastroFlex2 = <T extends CadastroAlunoInput | CadastroProfessorInput>({
+  opcao1,
+  opcao2,
+  infos,
+  setInfos,
+}: CadastroFlex2Prop<T>) => {
+  // Processa a opção recebida e retorna o resultado conforme o contexto de criação de alunos ou professores.
+  const tipo1 = IdentificarTipo(opcao1) as keyof T;
+  const tipo2 = IdentificarTipo(opcao2) as keyof T;
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    Escolhas: keyof T
+  ) => {
+    let texto = e.target.value;
+    if (Escolhas === "cpf") texto = formatCPF(texto);
+
+    if (Escolhas === "telefone" || Escolhas === "telefoneEmergencia")
+      texto = formatTelefone(texto);
+
+    setInfos((prev) => ({
+      ...prev,
+      [Escolhas]: texto as T[keyof T],
+    }));
+  };
+
+  // Classes utilitárias do Tailwind utilizadas para estilização dos campos de entrada.
+  const baseClass =
+    "w-full py-3 px-4 bg-(--bg-input) border-2 border-(--border-color) rounded-[10px] text-(--text-primary) text-[14px] focus:outline-none focus:border-(--primary-color)";
+
+  // Define os tipos de informações exibidas no select com base no tipo fornecido.
+  const selectOptions: Record<string, string[]> = {
+    status: ["Ativo", "Inativo", "Suspenso"],
+    turma: ["Selecionar Turma", "1A", "2B", "3C"],
+  };
+
+  // Define os tipos de informações exibidas no select com base no tipo fornecido.
+  const inputTypes: Record<string, string> = {
+    nascimento: "date",
+    contratacao: "date",
+    email: "email",
+    cpf: "text",
+    telefone: "text",
+  };
+
+  // Cria o Select ou Input para ser demonstrado
+  const TipoDiv = (Escolhas: keyof T) => {
+    const valor: string = infos[Escolhas] as string;
+
+    if (selectOptions[String(Escolhas)]) {
+      return (
+        <select
+          value={valor}
+          onChange={(e) => handleChange(e, Escolhas)}
+          className={baseClass}
+          id={String(Escolhas)}
+          name={String(Escolhas)}
+          required
+        >
+          {selectOptions[String(Escolhas)].map((opcao) => (
+            <option key={opcao} value={opcao}>
+              {opcao}
+            </option>
+          ))}
+        </select>
+      );
+    }
+
+    const type: string = inputTypes[String(Escolhas)] ?? "text";
+    const apenasLer: boolean =
+      Escolhas === "codigo" || Escolhas === "matricula";
+
+    return apenasLer ? (
+      <input
+        value={valor}
+        onChange={(e) => handleChange(e, Escolhas)}
+        type={type}
+        className={baseClass}
+        id={String(Escolhas)}
+        name={String(Escolhas)}
+        required
+        readOnly
+      />
+    ) : (
+      <input
+        value={valor}
+        onChange={(e) => handleChange(e, Escolhas)}
+        type={type}
+        className={baseClass}
+        id={String(Escolhas)}
+        name={String(Escolhas)}
+        required
+      />
+    );
+  };
+
+  return (
+    <div className="grid grid-cols-2 gap-5 mb-5">
+      <div className="flex flex-col gap-2">
+        <label
+          className="text-[14px] font-semibold text-(--text-primary)"
+          htmlFor={String(tipo1)}
+        >
+          {opcao1} <span className="text-(--red) ml-0.5">*</span>
+        </label>
+        {TipoDiv(tipo1)}
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label
+          className="text-[14px] font-semibold text-(--text-primary)"
+          htmlFor={String(tipo2)}
+        >
+          {opcao2} <span className="text-(--red) ml-0.5">*</span>
+        </label>
+        {TipoDiv(tipo2)}
+      </div>
+    </div>
+  );
+};
+
+export default CadastroFlex2;
