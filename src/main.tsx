cbc312dc2c3d757c1ee780edf312cd7c1338ca4d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 14:36:44 -0300

    Limpando o Main e deixando os providers serem carregados pelo theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext/index.tsx";
-import { CadastroAlunoProvider } from "./context/CadastroAlunoContext/index.tsx";
-import { CadastroProfessorProvider } from "./context/CadastroProfessorContext/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <CadastroAlunoProvider>
-        <CadastroProfessorProvider>
-          <App />
-        </CadastroProfessorProvider>
-      </CadastroAlunoProvider>
+      <App />
     </ThemeProvider>
   </StrictMode>
 );
