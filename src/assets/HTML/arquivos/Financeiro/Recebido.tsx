1d00865a11447b3fde81d1d02d62f7b018148278
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 09:47:40 -0300

    Criação dos Icones svg do financeiro

---
+++ src/assets/HTML/arquivos/Financeiro/Recebido.tsx
@@ -0,0 +1,16 @@
+export const Recebido = () => {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+    </svg>
+  );
+};
