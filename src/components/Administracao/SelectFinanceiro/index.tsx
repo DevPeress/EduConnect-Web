7f65366941094e9c3134792ee08e8c6e27ca5e4f
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 10:12:07 -0300

    Ajustando Select Financeiro com os Dados de Cada tabela

--- src/components/Administracao/SelectFinanceiro/index.tsx
+++ src/components/Administracao/SelectFinanceiro/index.tsx
@@ -1,27 +1,50 @@
-import type { SelectProps } from "../../../types/types";
+interface SelectProps {
+  Categoria: (valor: string) => void;
+  Meses: (valor: string) => void;
+  Status: (valor: string) => void;
+}
 
-const SelectAlunos = ({ salas, selecionada, status }: SelectProps) => {
+const SelectFinanceiro = ({ Categoria, Meses, Status }: SelectProps) => {
+  const Opcoes = (tipo: string) => {
+    switch (tipo) {
+      case "Status":
+        return ["Todos os Status", "Pago", "Pendente", "Atrasado", "Cancelado"];
+      case "Categorias":
+        return ["Todas as Categorias", "Mensalidade", "Material", "Extra"];
+      case "Meses":
+        return ["Todos os Meses", "Janeiro"];
+      default:
+        return [];
+    }
+  };
   return (
     <>
       <select
-        onChange={(e) => selecionada(e.target.value)}
+        onChange={(e) => Status(e.target.value)}
         className="bg-(--bg-input) border-2 border-(--border-color) rounded-[10px] py-2.5 px-3.5 text-(--text-primary) text-[14px] cursor-pointer min-w-[180px] hover:border-(--border-light)"
       >
-        {salas.map((item) => (
+        {Opcoes("Status").map((item) => (
           <option key={item}>{item}</option>
         ))}
       </select>
       <select
-        onChange={(e) => status(e.target.value)}
+        onChange={(e) => Categoria(e.target.value)}
         className="bg-(--bg-input) border-2 border-(--border-color) rounded-[10px] py-2.5 px-3.5 text-(--text-primary) text-[14px] cursor-pointer min-w-[180px] hover:border-(--border-light)"
       >
-        <option>Todos os Status</option>
-        <option>Ativo</option>
-        <option>Inativo</option>
-        <option>Suspenso</option>
+        {Opcoes("Categorias").map((item) => (
+          <option key={item}>{item}</option>
+        ))}
+      </select>
+      <select
+        onChange={(e) => Meses(e.target.value)}
+        className="bg-(--bg-input) border-2 border-(--border-color) rounded-[10px] py-2.5 px-3.5 text-(--text-primary) text-[14px] cursor-pointer min-w-[180px] hover:border-(--border-light)"
+      >
+        {Opcoes("Meses").map((item) => (
+          <option key={item}>{item}</option>
+        ))}
       </select>
     </>
   );
 };
 
-export default SelectAlunos;
+export default SelectFinanceiro;
