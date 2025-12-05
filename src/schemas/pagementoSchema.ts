473c04063fedb7a545badd212c92b70ec1717db4
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-05 09:45:01 -0300

    Criação do Schema dos Pagamentos

---
+++ src/schemas/pagementoSchema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const cadastroPagamentoSchema = z.object({
+  aluno: z.string(),
+  descricao: z.string().min(5, "Descrição inválida!"),
+  valor: z.number().min(1, "Valor inválido!"),
+  observacoes: z.string(),
+  categoria: z
+    .string()
+    .refine(
+      (value) => value !== "Selecionar categoria",
+      "Selecione uma categoria válida!"
+    ),
+  statuspagamento: z
+    .string()
+    .refine(
+      (value) => value !== "Selecionar status",
+      "Selecione um status válido!"
+    ),
+  metodo: z
+    .string()
+    .refine(
+      (value) => value !== "Selecionar método",
+      "Selecione um método válido!"
+    ),
+  vencimento: z.string(),
+  datapagamento: z.string(),
+});
+
+export type CadastroPagamentoInput = z.infer<typeof cadastroPagamentoSchema>;
