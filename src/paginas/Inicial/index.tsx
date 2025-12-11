c39cef6ab0b98a5a73645cd894abce8a860c111d
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-11 11:10:09 -0300

    Trocando tipagem estática na página e importando do types

--- src/paginas/Inicial/index.tsx
+++ src/paginas/Inicial/index.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Options } from "../../utils/paginação";
+import type { AuthPaginas } from "../../types/types";
 
-const Inicial = ({ Logado, Cargo }: { Logado: boolean; Cargo: string }) => {
+const Inicial = ({ logado, cargo }: AuthPaginas) => {
   const navegar = useNavigate();
 
   useEffect(() => {
-    if (!Logado) {
+    if (!logado) {
       navegar("/login");
       return;
     }
 
-    const opcao = Options.find((option) => option.cargos?.includes(Cargo));
+    const opcao = Options.find((option) => option.cargos?.includes(cargo));
     navegar(opcao ? opcao.pagina : "/not-authorized");
-  }, [Logado, Cargo, navegar]);
+  }, [logado, cargo, navegar]);
 
   return null;
 };
