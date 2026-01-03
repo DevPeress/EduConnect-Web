171c07f1046bf884087f42ac2e355492b02e15ab
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 10:39:04 -0300

    Adicionando requisição de departamentos

--- src/paginas/Administracao/Funcionarios/index.tsx
+++ src/paginas/Administracao/Funcionarios/index.tsx
@@ -17,10 +17,13 @@ const FuncionariosAdmin = () => {
     const cargo = localStorage.getItem("Exibir");
     return cargo ? true : false;
   });
-  const [selecionada, setSelecionada] = useState<string>("Todos os Departamentos");
+  const [selecionada, setSelecionada] = useState<string>(
+    "Todos os Departamentos"
+  );
   const [status, setStatus] = useState<string>("Todos os Status");
   const [ano, setAno] = useState<string>("Todos os Anos");
   const [funcionarios, setFuncionarios] = useState<Funcionario[]>([]);
+  const [departamentos, setDepartamentos] = useState<string[]>([]);
   const [anos, setAnos] = useState<string[]>([]);
   const [total, setTotal] = useState<number>(0);
   const [pagina, setPagina] = useState<number>(1);
@@ -28,7 +31,9 @@ const FuncionariosAdmin = () => {
   // Requisita os dados novos toda vez que status, categoria ou meses mudar
   const Pesquisa = () => {
     http
-      .get(`api/funcionarios/filtro/selecionada/${selecionada}/status/${status}/page/${pagina}/ano/${ano}`)
+      .get(
+        `api/funcionarios/filtro/selecionada/${selecionada}/status/${status}/page/${pagina}/ano/${ano}`
+      )
       .then(function (dados) {
         setTotal(dados.data.total);
         setFuncionarios(dados.data.dados);
@@ -51,8 +56,9 @@ const FuncionariosAdmin = () => {
   }, [total]);
 
   useEffect(() => {
-    http.get("api/funcionarios/pegarAnos").then(function (dados) {
-      setAnos(dados.data);
+    http.get("api/funcionarios/pegarInformativos").then(function (dados) {
+      setAnos(dados.data.anos);
+      setDepartamentos(dados.data.departamentos)
     });
   }, []);
 
@@ -78,11 +84,12 @@ const FuncionariosAdmin = () => {
       <div className="flex justify-between items-center gap-5 mb-6 flex-wrap">
         <div className="flex gap-3 flex-wrap">
           <Selects
-            selecionadoCategorias={setSelecionada}
+            selecionadoDepartamento={setSelecionada}
             selecionadoStatus={setStatus}
             selecionadoAno={setAno}
             tipo="Funcionarios"
             anos={anos}
+            departamento={departamentos}
           />
         </div>
 
