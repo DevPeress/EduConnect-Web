1ef38d7eafa8f34f223e10d1313d40d2189d443a
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 15:01:01 -0300

    Adicionado cadastroFuncionario para export

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,4 +1,5 @@
 import { useCadastroAluno } from "./CadastroAlunoContext";
+import { useCadastroFuncionario } from "./CadastroFuncionarioContext";
 import { useCadastroPagamento } from "./CadastroPagamentoContext";
 import { useCadastroProfessor } from "./CadastroProfessorContext";
 import { useCadastroTurma } from "./CadastroTurmaContext";
@@ -8,11 +9,13 @@ export function useCadastroMenu() {
   const cadastroAluno = useCadastroAluno().openMenu;
   const cadastroPagamento = useCadastroPagamento().openMenu;
   const cadastroTurma = useCadastroTurma().openMenu;
+  const cadastroFuncionario = useCadastroFuncionario().openMenu;
 
   return {
     cadastroProfessor,
     cadastroAluno,
     cadastroPagamento,
     cadastroTurma,
+    cadastroFuncionario,
   };
 }
