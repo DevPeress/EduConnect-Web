c636cb8e0f1e2b69c279dcde0689512c34e7ed65
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 22:29:45 -0300

    Valores do schema criados

--- src/schemas/turmaSchema.ts
+++ src/schemas/turmaSchema.ts
@@ -1,20 +1,23 @@
 import { z } from "zod";
 
-export const cadastroAlunoSchema = z.object({
-    matricula: z.string(),
-    status: z.string(),
-    nome: z.string().min(3, "Nome deve ter no mínimo 3 caracteres!"),
-    cpf: z.string().min(14, "CPF inválido!"),
-    turma: z.string().refine(
-        (value) => value !== "Selecionar Turma",
-        "Selecione uma turma válida!"
+export const cadastroTurmaSchema = z.object({
+  matricula: z.string(),
+  status: z.string(),
+  nome: z.string().min(3, "Nome deve ter no mínimo 3 caracteres!"),
+  ano: z.string().min(3, "Ano Letivo inválido!"),
+  turno: z
+    .string()
+    .refine(
+      (value) => value !== "Selecionar o Turno",
+      "Selecione um turno válido!"
     ),
-    email: z.string().email("E-mail inválido!"),
-    telefone: z.string().min(14, "Número de Telefone inválido!"),
-    endereco: z.string().min(5, "Endereço inválido!"),
-    nascimento: z.string("Data de Nascimento Inválida!"),
-    nomeEmergencia: z.string().min(3, "Nome do Contato de Emergência deve ter no mínimo 3 caracteres!"),
-    telefoneEmergencia: z.string().min(14, "Número do Contato de Emergência inválido!")
-})
+  sala: z.string().min(2, "Sala inválida!"),
+  capacidade: z.number(),
+  professor: z.string().min(5, "Professor inválido!"),
+  inicio: z.string(),
+  fim: z.string(),
+  dias: z.string(),
+  disciplinas: z.string(),
+});
 
-export type CadastroAlunoInput = z.infer<typeof cadastroAlunoSchema>;
+export type CadastroTurmaInput = z.infer<typeof cadastroTurmaSchema>;
