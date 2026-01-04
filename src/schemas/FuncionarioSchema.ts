7d97fd7186ac5ecb36b0a0b28893a3b0d73d9807
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 14:49:22 -0300

    Criando do Schema para funcionários

---
+++ src/schemas/FuncionarioSchema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const cadastroFuncionarioSchema = z.object({
+  registro: z.string(),
+  nome: z.string().min(3, "Nome deve ter no mínimo 3 caracteres"),
+  email: z.string().email("E-mail inválido!"),
+  telefone: z.string().min(14, "Número de Telefone inválido!"),
+  status: z.string(),
+  nasc: z.string(),
+  endereco: z.string().min(5, "Endereço inválido!"),
+  cpf: z.string().min(14, "CPF inválido!"),
+  telefoneEmergencia: z
+    .string()
+    .min(14, "Número do Contato de Emergência inválido!"),
+  cargo: z.string(),
+  departamento: z.string(),
+  supervisor: z.string(),
+  turno: z.string(),
+  contratacao: z.string(),
+});
+
+export type CadastroFuncionarioInput = z.infer<
+  typeof cadastroFuncionarioSchema
+>;
