b45c78c04dee06193260d2d794db57a0b3f9988b
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 22:34:48 -0300

    Adicionando export de cadastro de turma

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,15 +1,18 @@
 import { useCadastroAluno } from "./CadastroAlunoContext";
 import { useCadastroPagamento } from "./CadastroPagamentoContext";
 import { useCadastroProfessor } from "./CadastroProfessorContext";
+import { useCadastroTurma } from "./CadastroTurmaContext";
 
 export function useCadastroMenu() {
-    const cadastroProfessor = useCadastroProfessor().openMenu;
-    const cadastroAluno = useCadastroAluno().openMenu;
-    const cadastroPagamento = useCadastroPagamento().openMenu;
+  const cadastroProfessor = useCadastroProfessor().openMenu;
+  const cadastroAluno = useCadastroAluno().openMenu;
+  const cadastroPagamento = useCadastroPagamento().openMenu;
+  const cadastroTurma = useCadastroTurma().openMenu;
 
-    return {
-        cadastroProfessor,
-        cadastroAluno,
-        cadastroPagamento
-    };
+  return {
+    cadastroProfessor,
+    cadastroAluno,
+    cadastroPagamento,
+    cadastroTurma,
+  };
 }
