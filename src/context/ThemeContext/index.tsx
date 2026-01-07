0796fd6485978f8f2b838a7c5b6333ac39ae601e
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-07 13:11:07 -0300

    Importando o provider para o sistema

--- src/context/ThemeContext/index.tsx
+++ src/context/ThemeContext/index.tsx
@@ -8,6 +8,7 @@ import { CadastroPagamentoProvider } from "../Cadastros/CadastroPagamentoContext
 import { CadastroTurmaProvider } from "../Cadastros/CadastroTurmaContext";
 import { CadastroFuncionarioProvider } from "../Cadastros/CadastroFuncionarioContext";
 import { CadastroDisciplinasProvider } from "../Cadastros/CadastroDisciplinasContext";
+import { EditarFuncionarioProvider } from "../Editar/EditarFuncionarioContext";
 
 const ThemeContext = createContext<ThemeType | undefined>(undefined);
 
@@ -39,7 +40,9 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
             <CadastroTurmaProvider>
               <CadastroFuncionarioProvider>
                 <CadastroDisciplinasProvider>
-                  {children}
+                  <EditarFuncionarioProvider>
+                    {children}
+                  </EditarFuncionarioProvider>
                 </CadastroDisciplinasProvider>
               </CadastroFuncionarioProvider>
             </CadastroTurmaProvider>
