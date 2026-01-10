f5a4fe31cf66e4428d06acd14226f5c84f48f7d1
Fabrício Peres <fabricioperesdsantos@gmail.com>
2026-01-10 12:39:52 -0300

    Alterando o ExibirPesquisa para receber os dados

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,15 @@ import { useAuth } from "../../context/AuthContext";
 import { useTheme } from "../../context/ThemeContext";
 import { http } from "../../utils/axios";
 
-const Header = ({ exibirPesquisa }: { exibirPesquisa: boolean }) => {
+const Header = ({
+  exibirPesquisa,
+}: {
+  exibirPesquisa: {
+    exibir: boolean;
+    valor?: string;
+    set?: (valor: string) => void;
+  };
+}) => {
   const tema = useTheme();
   const dark = tema.dark;
   const auth = useAuth();
@@ -43,49 +51,51 @@ const Header = ({ exibirPesquisa }: { exibirPesquisa: boolean }) => {
   return (
     <header className="bg-(--bg-sidebar) border-b-2 border-(--border-color) px-9.5 h-19.25 flex justify-between items-center sticky top-0 z-10">
       <div className="flex items-center gap-4 flex-1">
-        {exibirPesquisa ? (
-          <>
-            <button className="flex bg-transparent border-none text-(--text-secondary) cursor-pointer p-4 border-2">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="3" y1="12" x2="21" y2="12"></line>
-                <line x1="3" y1="6" x2="21" y2="6"></line>
-                <line x1="3" y1="18" x2="21" y2="18"></line>
-              </svg>
-            </button>
-            <div className="relative flex-1 max-w-[500px]">
-              <svg
-                className="absolute left-3.5 top-[50%] -translate-y-[50%] text-(--text-muted) pointer-events-none"
-                width="18"
-                height="18"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.35-4.35"></path>
-              </svg>
-              <input
-                className="w-full bg-(--bg-input) border-4 border-(--border-color) pt-3 pr-4 pb-3 pl-10 text-(--text-primary) text-[14px] focus:outline-none focus:border-(--primary-color) focus:bg-(--bg-card)"
-                type="text"
-                placeholder="Buscar por nome ou matrícula..."
-              />
-            </div>
-          </>
-        ) : (
-          null
-        )}
+        {exibirPesquisa.exibir &&
+          exibirPesquisa.set &&
+          exibirPesquisa.valor && (
+            <>
+              <button className="flex bg-transparent border-none text-(--text-secondary) cursor-pointer p-4 border-2">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="3" y1="12" x2="21" y2="12"></line>
+                  <line x1="3" y1="6" x2="21" y2="6"></line>
+                  <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg>
+              </button>
+              <div className="relative flex-1 max-w-[500px]">
+                <svg
+                  className="absolute left-3.5 top-[50%] -translate-y-[50%] text-(--text-muted) pointer-events-none"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <circle cx="11" cy="11" r="8"></circle>
+                  <path d="m21 21-4.35-4.35"></path>
+                </svg>
+                <input
+                  value={exibirPesquisa.valor}
+                  onChange={(e) => exibirPesquisa.set?.(e.target.value)}
+                  className="w-full bg-(--bg-input) border-4 border-(--border-color) pt-3 pr-4 pb-3 pl-10 text-(--text-primary) text-[14px] focus:outline-none focus:border-(--primary-color) focus:bg-(--bg-card)"
+                  type="text"
+                  placeholder="Buscar por nome ou matrícula..."
+                />
+              </div>
+            </>
+          )}
       </div>
 
       <div className="flex items-center gap-4">
