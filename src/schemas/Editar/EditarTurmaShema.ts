9b812fd5480e0a18296e5cba4b0c2c531e93e0c3
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-16 14:28:01 -0300

    Criação do Schema para editar dados da turma

---
+++ src/schemas/Editar/EditarTurmaShema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const editarTurmaSchema = z.object({
+  codigo: z.string(),
+  nome: z.string().min(3, "Nome deve ter no mínimo 3 caracteres"),
+  turno: z
+    .string()
+    .refine(
+      (value) => value !== "Selecionar o Turno",
+      "Selecione um turno válido!"
+    ),
+  status: z.string(),
+  ano: z.string().min(3, "Ano Letivo inválido!"),
+  capacidade: z.number(),
+  professor: z.string().min(5, "Professor inválido!"),
+  inicio: z.string(),
+  fim: z.string(),
+  sala: z.string().min(2, "Sala inválida!"),
+  dias: z.array(z.string()),
+  disciplinas: z.array(z.string()),
+  alunos: z.array(z.string()),
+});
+
+export type EditarTurmaInput = z.infer<typeof editarTurmaSchema>;
