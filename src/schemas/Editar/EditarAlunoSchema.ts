50f437b0a27acc992924114cc9cb3ac07a390b48
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-21 16:25:00 -0300

    Ajustando os valores do zod para editar aluno

--- src/schemas/Editar/EditarAlunoSchema.ts
+++ src/schemas/Editar/EditarAlunoSchema.ts
@@ -1,20 +1,26 @@
 import { z } from "zod";
 
 export const editarAlunoSchema = z.object({
-    matricula: z.string(),
-    status: z.string(),
-    nome: z.string().min(3, "Nome deve ter no mínimo 3 caracteres!"),
-    cpf: z.string().min(14, "CPF inválido!"),
-    turma: z.string().refine(
-        (value) => value !== "Selecionar Turma",
-        "Selecione uma turma válida!"
+  matricula: z.string(),
+  nome: z.string().min(3, "Nome deve ter no mínimo 3 caracteres!"),
+  email: z.string().email("E-mail inválido!"),
+  telefone: z.string().min(14, "Número de Telefone inválido!"),
+  status: z.string(),
+  nascimento: z.string("Data de Nascimento Inválida!"),
+  endereco: z.string().min(5, "Endereço inválido!"),
+  cpf: z.string().min(14, "CPF inválido!"),
+  nomeEmergencia: z
+    .string()
+    .min(3, "Nome do Contato de Emergência deve ter no mínimo 3 caracteres!"),
+  telefoneEmergencia: z
+    .string()
+    .min(14, "Número do Contato de Emergência inválido!"),
+  turma: z
+    .string()
+    .refine(
+      (value) => value !== "Selecionar Turma",
+      "Selecione uma turma válida!",
     ),
-    email: z.string().email("E-mail inválido!"),
-    telefone: z.string().min(14, "Número de Telefone inválido!"),
-    endereco: z.string().min(5, "Endereço inválido!"),
-    nascimento: z.string("Data de Nascimento Inválida!"),
-    nomeEmergencia: z.string().min(3, "Nome do Contato de Emergência deve ter no mínimo 3 caracteres!"),
-    telefoneEmergencia: z.string().min(14, "Número do Contato de Emergência inválido!")
-})
+});
 
 export type EditarAlunoInput = z.infer<typeof editarAlunoSchema>;
