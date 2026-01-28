59cf887436773a3ccf950db85a75fd1878244620
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-28 14:29:07 -0300

    Inserindo opção de abrir o editar de professor

--- src/context/index.ts
+++ src/context/index.ts
@@ -6,6 +6,7 @@ import { useCadastroProfessor } from "./Cadastros/CadastroProfessorContext";
 import { useCadastroTurma } from "./Cadastros/CadastroTurmaContext";
 import { useEditarAluno } from "./Editar/EditarAlunoContext";
 import { useEditarFuncionario } from "./Editar/EditarFuncionarioContext";
+import { useEditarProfessor } from "./Editar/EditarProfessorSchema";
 import { useEditarTurma } from "./Editar/EditarTurmaContext";
 
 export function useCadastroMenu() {
@@ -30,11 +31,14 @@ export function useEditarMenu() {
   const editarFuncionarioCtx = useEditarFuncionario();
   const editarTurmaCtx = useEditarTurma();
   const editarAlunoCtx = useEditarAluno();
+  const editarProfessorCtx = useEditarProfessor();
 
   return {
     editarFuncionario: (registro: string) =>
       editarFuncionarioCtx.openMenu(registro),
     editarTurma: (registro: string) => editarTurmaCtx.openMenu(registro),
     editarAluno: (registro: string) => editarAlunoCtx.openMenu(registro),
+    editarProfessor: (registro: string) =>
+      editarProfessorCtx.openMenu(registro),
   };
 }
