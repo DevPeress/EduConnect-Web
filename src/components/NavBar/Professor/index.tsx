868ef01aed4f2e7dde9ab7342a13285eb761909b
Fabrício <fabricioperesdsantos@gmail.com>
2026-02-12 10:03:54 -0300

    Criação das abas de professores e alunos

---
+++ src/components/NavBar/Professor/index.tsx
@@ -0,0 +1,63 @@
+import {
+  NavDashBoard,
+  NavAlunos,
+  NavTurmas,
+  NavNotas,
+  NavCalendario,
+} from "../../../assets/HTML";
+import type { Nav } from "../../../types/types";
+import LinkPaginas from "../LinkPaginas/LinkPaginas";
+
+const Paginas: Nav[] = [
+  { nome: "DashBoard", pagina: "/professores/dashboard", svg: NavDashBoard },
+  { nome: "Alunos", pagina: "/professores/alunos", svg: NavAlunos },
+  { nome: "Turmas", pagina: "/professores/turmos", svg: NavTurmas },
+  { nome: "Notas", pagina: "/professores/notas", svg: NavNotas },
+  {
+    nome: "Calendário",
+    pagina: "/admin/calendario",
+    svg: NavCalendario,
+  }
+];
+
+const NavFuncionario = ({ pagina }: { pagina: string }) => {
+  return (
+    <nav className="flex-1 px-3.5 py-3.5">
+      <div className="mb-4">
+        <span className="block text-[11px] font-semibold text-(--text-muted) uppercase leading-[0.5px] px-2 py-1 mb-5">
+          Principal
+        </span>
+        {Paginas.slice(0, 4).map((item) => {
+          const paginaAtual: boolean = pagina === item.pagina;
+
+          return (
+            <LinkPaginas
+              key={item.nome}
+              item={item}
+              paginaAtual={paginaAtual}
+            />
+          );
+        })}
+      </div>
+
+      <div>
+        <span className="block text-[11px] font-semibold text-(--text-muted) uppercase leading-[0.5px] px-2 py-1 mb-1">
+          Acadêmico
+        </span>
+        {Paginas.slice(5, 8).map((item) => {
+          const paginaAtual: boolean = pagina === item.pagina;
+
+          return (
+            <LinkPaginas
+              key={item.nome}
+              item={item}
+              paginaAtual={paginaAtual}
+            />
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default NavFuncionario;
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -14,6 +14,10 @@ const NavBar = () => {
     <NavAdministrador pagina={pagina} />
   ) : cargo === "Funcionário" ? (
     <NavFuncionario pagina={pagina} />
+  ) : cargo === "Professor" ? (
+    <NavFuncionario pagina={pagina} />
+  ) : cargo === "Aluno" ? (
+    <NavFuncionario pagina={pagina} />
   ) : null;
 };
 
