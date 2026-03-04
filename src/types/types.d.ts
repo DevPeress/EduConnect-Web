51c8073c4e266932d61b4f7a41a9b90264f430de
Fabrício <fabricioperesdsantos@gmail.com>
2026-03-04 16:00:24 -0300

    Adicionando mais tipos de registros

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -81,6 +81,11 @@ export enum AuditAction {
   Login = 4,
   Logout = 5,
   AccessDenied = 6,
+  Nota = 7,
+  Presenca = 8,
+  NovoAluno = 9,
+  Reuniao = 10,
+  Material = 11,
 }
 
 export interface AtividadeType {
