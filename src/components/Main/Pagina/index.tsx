383eeb8975a5d4edb464fe7ef3adf172d6e2c4a3
Fabrício Peres <fabricioperesdsantos@gmail.com>
2026-03-17 08:17:47 -0300

    Colocando verificação para ver botão de adicionar

--- src/components/Main/Pagina/index.tsx
+++ src/components/Main/Pagina/index.tsx
@@ -1,6 +1,11 @@
+import { useAuth } from "../../../context";
 import type { PaginaMainProps } from "../../../types/types";
 
 const PaginaMain = ({ titulo, desc, botao, children }: PaginaMainProps) => {
+  const auth = useAuth();
+
+  const cargo: string = auth.cargo;
+
   return (
     <div className="py-8 px-8 max-w-400 m-0 mx-auto w-full">
       <div className="flex justify-between mb-5">
@@ -11,30 +16,32 @@ const PaginaMain = ({ titulo, desc, botao, children }: PaginaMainProps) => {
           <p className="text-[15px] text-(--text-secondary)">{desc}</p>
         </div>
 
-        <button
-          onClick={botao.adicionar}
-          className="items-center gap-2.5 py-3 px-6 bg-(--primary-color) text-white border-none rounded-[10px] text-[14px] cursor-pointer hover:bg-(--primary-hover) hover:-translate-y-0.5"
-          style={{
-            boxShadow: " 0 4px 12px rgba(59, 130, 246, 0.3)",
-            display: botao.ativo ? "flex" : "none",
-          }}
-        >
-          <svg
-            className="shrink-0"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+        {(cargo === "Administrador" || cargo === "Funcionário") && (
+          <button
+            onClick={botao.adicionar}
+            className="items-center gap-2.5 py-3 px-6 bg-(--primary-color) text-white border-none rounded-[10px] text-[14px] cursor-pointer hover:bg-(--primary-hover) hover:-translate-y-0.5"
+            style={{
+              boxShadow: " 0 4px 12px rgba(59, 130, 246, 0.3)",
+              display: botao.ativo ? "flex" : "none",
+            }}
           >
-            <line x1="12" y1="5" x2="12" y2="19"></line>
-            <line x1="5" y1="12" x2="19" y2="12"></line>
-          </svg>
-          {botao.mensagem}
-        </button>
+            <svg
+              className="shrink-0"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="12" y1="5" x2="12" y2="19"></line>
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+            {botao.mensagem}
+          </button>
+        )}
       </div>
 
       {children}
